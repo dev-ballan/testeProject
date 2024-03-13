dceaf8195242bddf20b8f66b688cb24a00e1044e
Allan Baad <munisallan@gmail.com>
2024-03-13 19:11:38 -0300

    Test inserção do backend

---
+++ src/main/java/com/generation/sustentatech/model/Categoria.java
@@ -0,0 +1,72 @@
+package com.generation.sustentatech.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Tipo não pode ser vazio e nem ser um espaço em branco!")
+	@Size(min = 5, max = 255, message = "No mínimo 5 e máximo 255 caracteres!")
+	private String tipo;
+	
+	@NotNull(message = "O atributo Descrição não pode ser vazio!")
+	@Size(max = 1000, message = "No máximo 1000 caracteres!")
+	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	
+}
+
